c24ff51d9aae8959c8d144043891278e8fc2b1ae
Amusat Gift Nafisat <amusatgifty@gmail.com>
2025-10-17 09:43:29 +0100

    api update

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", 
+  baseURL: "https://portback-06e7.onrender.com/api", 
 });
 
 // Automatically attach the token (if logged in)
